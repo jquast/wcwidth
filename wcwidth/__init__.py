1db89ce9230fa4cd71a2d38528e8c433de0b874d
jquast <contact@jeffquast.com>
2014-04-06 20:45:59 -0700

    export wcwidth_cjk

--- wcwidth/__init__.py
+++ wcwidth/__init__.py
@@ -1,3 +1,9 @@
-from .wcwidth import wcwidth, wcswidth, rpadstring, truncatestring
+from .wcwidth import (
+    wcwidth, wcswidth,
+    wcwidth_cjk, wcswidth_cjk,
+    rpadstring, truncatestring
+)
 
-__all__ = ['wcwidth', 'wcswidth', 'rpadstring', 'truncatestring']
+__all__ = ['wcwidth', 'wcswidth',
+           'wcwidth_cjk', 'wcswidth_cjk',
+           'rpadstring', 'truncatestring']
