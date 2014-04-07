1d3d264a7c3f4e0104b72ae122daa54597caf65e
jquast <contact@jeffquast.com>
2014-04-07 09:11:19 -0700

    add wcwidth_cjk support ('a') for ambiguous
    as well as a few other display fixes. Still working on piping output to
    less(1) instead of the internal thingie.

--- test-display.py
+++ test-display.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import division
-from wcwidth import wcwidth
+from wcwidth import wcwidth, wcwidth_cjk
 from blessed import Terminal
 from functools import partial
 import unicodedata
@@ -17,8 +17,13 @@ try:
     flushout('')
 except TypeError as err:
     assert "'flush' is an invalid keyword argument" in err.args[0]
-    import sys
-    flushout = lambda val: sys.stdout.flush()
+
+    def flushout():
+        import sys
+        try:
+            sys.stdout.flush()
+        except BrokenPipeError:
+            pass
 
 
 try:
@@ -39,22 +44,33 @@ except NameError as err:
 
 # END, python 2.6 - 3.3 compatibilities
 
+# some poor python builds (apple, etc.) are narrow, presumably
+# for smaller memory footprint of character strings.
+try:
+    _ = unichr(0x10000)
+    LIMIT_UCS = 0x3fffd
+except ValueError as err:
+    assert 'narrow Python build' in err.args[0], err.args
+    LIMIT_UCS = 0x10000
+    import time
+    warnings.warn('narrow Python build, only a small subset of '
+                  'characters may be represented.')
+    time.sleep(3)
+
 
 class WcWideCharacterGenerator(object):
     " generates unicode characters of the presumed terminal width "
-    def __init__(self, width=2):
-        try:
-            _ = unichr(0x10000)
-            LIMIT_UCS = 0x3fffd
-        except ValueError as err:
-            assert 'narrow Python build' in err.args[0], err.args
-            LIMIT_UCS = 0x10000
+    def __init__(self, width=2, cjk=False):
+        """
+        ``cjk``
+          Treat ambiguous-width characters as double-width
+        """
         self.characters = (unichr(idx)
                            for idx in xrange(LIMIT_UCS)
-                           if wcwidth(unichr(idx)) == width
+                           if cjk and wcwidth_cjk(unichr(idx)) == width
+                           or not cjk and wcwidth(unichr(idx)) == width
                            and not unicodedata.combining(unichr(idx))
                            )
-        self.width = width
 
     def __next__(self):
         while True:
@@ -73,7 +89,6 @@ class Style(object):
     " decorator for screen output "
     heading = lambda self, text: text
     hint = lambda self, text: text
-    status = lambda self, text: text
     delimiter = u'|'
     continuation = u' ..'
     header_hint = u'-'
@@ -90,12 +105,14 @@ class Style(object):
 class Screen(object):
     " represents terminal and data dimensions "
     intro_msg_fmt = (u'Characters {wide} terminal cells wide. '
+                     u'{ambiguity}'
                      u'Delimiters ({delim}) should align.')
 
-    def __init__(self, term, style, wide=2):
+    def __init__(self, term, style, wide=2, cjk=False):
         self.term = term
         self.style = style
-        self.wide = 2
+        self.wide = wide
+        self.cjk = cjk
 
     @property
     def header(self):
@@ -114,20 +131,22 @@ class Screen(object):
     @property
     def head_item(self):
         heading = (u'{delimiter}{hint}{delimiter}'
-                   .format(delimiter=self.style.delimiter,
+                   .format(delimiter=self.style.hint(self.style.delimiter),
                            hint=self.style.header_hint * self.wide))
-        alignment = lambda text, *args: (
-            text.rjust(*args) if self.style.alignment == 'right' else
-            text.ljust(*args))
+        alignment = lambda *args: (
+            self.term.rjust(*args) if self.style.alignment == 'right' else
+            self.term.ljust(*args))
         txt = alignment(heading, self.hint_width, self.style.header_fill)
         return self.style.heading(txt)
 
     @property
     def msg_intro(self):
-        delim = self.style.heading(self.style.delimiter)
+        delim = self.style.hint(self.style.delimiter)
         wide = self.style.heading('{}'.format(self.wide))
-        return self.term.wrap(self.intro_msg_fmt
-                              .format(wide=wide, delim=delim))
+        ambiguity = u'' if not self.cjk else (
+            u'Ambiguous-width characters are double-width. ')
+        return self.term.wrap(self.intro_msg_fmt.format(
+            wide=wide, ambiguity=ambiguity, delim=delim))
 
     @property
     def row_ends(self):
@@ -169,17 +188,20 @@ class Pager(object):
         self.dirty = 2
         self._page_data.clear()
         self.last_page = None
-        self.character_generator = self.character_factory(self.screen.wide)
+        self.character_generator = self.character_factory(
+            self.screen.wide, self.screen.cjk)
 
     def page_data(self, idx, offset):
         size = self.screen.page_size
-        while offset < 0:
+        while offset < 0 and idx:
             offset += size
-            idx = max(0, idx - 1)
+            idx -= 1
+        offset = max(0, offset)
         while offset > size:
             offset -= size
-            idx = max(0, idx + 1)
-
+            idx += 1
+        if idx == self.last_page:
+            offset = 0
         _idx = -1
         while _idx <= idx:
             # retrieve new page data
@@ -209,7 +231,12 @@ class Pager(object):
     def run(self, writer, reader):
         page_idx = page_offset = 0
         if not self.term.is_a_tty:
-            self.draw(writer, page_idx, page_offset)
+            while True:
+                npage_idx, offset = self.draw(writer, page_idx + 1, page_offset)
+                if npage_idx == page_idx:
+                    break
+                page_idx = npage_idx
+            return
         while True:
             if self.dirty:
                 page_idx, page_offset = self.draw(writer,
@@ -230,8 +257,11 @@ class Pager(object):
         # exit
         if inp.lower() in (u'q', u'Q'):
             return (-1, -1)
-        elif inp in (u'1', u'2'):
-            if int(inp) != self.screen.wide:
+        elif inp in (u'1', u'2', u'a'):
+            if inp == u'a':
+                self.screen.cjk = not self.screen.cjk
+                self.on_resize(None, None)
+            elif int(inp) != self.screen.wide:
                 self.screen.wide = int(inp)
                 self.on_resize(None, None)
         elif inp in (u'_', u'-'):
@@ -289,7 +319,6 @@ class Pager(object):
             (idx, offset), data = self.page_data(idx, offset)
             for txt in self.page_view(data):
                 writer(txt)
-        writer('\n')
         self.draw_status(writer, idx)
         flushout()
         return idx, offset
@@ -306,33 +335,36 @@ class Pager(object):
     def draw_loading(self, writer, idx):
         if self.term.is_a_tty:
             if not idx in self._page_data:
-                writer(self.screen.style.status(u' '))
-                writer(self.screen.style.status(self.screen.style.loading))
+                writer(u' ' + self.screen.style.loading)
             else:
-                writer(self.screen.style.status(u'+'))
+                writer(u' +')
             flushout()
 
     def draw_status(self, writer, idx):
         if self.term.is_a_tty:
+            writer(self.term.move(self.term.height - 1))
             end=u' (END)' if idx == self.last_page else u''
-            writer(self.screen.style.status(
-                'Page {idx}{end}. q to quit, [kjfb-=]'.format(idx=idx, end=end)))
-            writer(self.term.clear_eos)
+            writer('Page {idx}{end}. q to quit, [keys: kjfb12-=]'
+                   .format(idx=idx, end=end))
 
     def page_view(self, data):
         if self.term.is_a_tty:
             yield self.term.move(self.screen.row_begins, 0)
+        clear_eol = self.term.clear_eol
+        clear_eos = self.term.clear_eos
+        num_cols = self.screen.num_columns
+
         col = row = 0
         for ucs, name in data:
             val = self.text_entry(ucs, name)
             col += 1
-            if col == self.screen.num_columns:
+            if col == num_cols:
                 col = 0
                 row += 1
-                val = u''.join((val, u'\n'))
-            yield val + self.term.clear_eol
-        if col != 0:
-            yield u'\n' + self.term.clear_eol
+                val = u''.join((val, clear_eol, u'\n'))
+            yield val
+        if self.term.is_a_tty:
+            yield u''.join((clear_eol, u'\n', clear_eos))
 
     def text_entry(self, ucs, name):
         style = self.screen.style
@@ -358,7 +390,6 @@ def main():
     term = Terminal()
     style = Style(heading=term.magenta,
                   hint=term.bright_cyan,
-                  status=term.bright_yellow,
                   delimiter=u'|',
                   ) if term.number_of_colors else Style()
     screen = Screen(term, style)
