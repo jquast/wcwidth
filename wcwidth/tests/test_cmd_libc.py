46ce3b725b3f814f5ecf67cb1c179aba076f06f3
jquast <contact@jeffquast.com>
2014-04-19 00:44:58 -0700

    brevity regarding assertions, etc.
    ignore combining from testing -- too many libc thinks they're width of 1
    when python is fully aware that they are combining. also anything
    without a name is very likely -1 for wcswidth(3), and for us, although
    we may measure it 1 or 2 (correctly), there isnt any point in testing
    them.

--- wcwidth/tests/test_cmd_libc.py
+++ wcwidth/tests/test_cmd_libc.py
@@ -12,16 +12,42 @@ not guaranteed to be successful on all platforms, especially
 where wcwidth(3)/wcswidth(3) is out of date.
 """
 # standard
-import locale
-import ctypes
+import unicodedata
 import ctypes.util
-import warnings
 import itertools
+import warnings
+import locale
+import sys
 
 # local
 import wcwidth
 
-# use chr() for py3.x, unichr ()for py2.x
+
+def is_named(ucs):
+    try:
+        return bool(unicodedata.name(ucs))
+    except ValueError:
+        return False
+
+
+def isnt_combining(ucs):
+    return not unicodedata.combining(ucs)
+
+
+def report_ucs_msg(ucs, wcwidth_libc, wcwidth_local):
+    ucp = (ucs.encode('unicode_escape')[2:]
+           .decode('ascii')
+           .upper()
+           .lstrip('0'))
+    url = "http://codepoints.net/U+{}".format(ucp)
+    name = unicodedata.name(ucs)
+    return ("libc={}, ours={}, name={}, url={} "
+            " --oo{}oo--{}".format(
+                wcwidth_libc, wcwidth_local,
+                name, url, ucs, (" :libc failed to identify combining"
+                                 if wcwidth_local == -1 else '')))
+
+# use chr() for py3.x, unichr() for py2.x
 try:
     _ = unichr(0)
 except NameError as err:
@@ -30,16 +56,17 @@ except NameError as err:
     else:
         raise
 
-# some poor python builds (apple, etc.) are narrow, presumably
-# for smaller memory footprint of character strings.
-try:
-    _ = unichr(0x10000)
-    LIMIT_UCS = 0x3fffd
-except ValueError as err:
-    assert 'narrow Python build' in err.args[0], err.args
-    LIMIT_UCS = 0x10000
+if sys.maxunicode < 1114111:
     warnings.warn('narrow Python build, only a small subset of '
                   'characters may be tested.')
+
+
+# Load the entire table into memory, excluding those values
+# that are not named, or are combining characters.
+ALL_UCS = [ucs for ucs in
+           [unichr(val) for val in range(sys.maxunicode)]
+           if is_named(ucs) and isnt_combining(ucs)
+           ]
 
 
 def test_hello_jp():
@@ -78,35 +105,34 @@ class Test_libc_width:
         assert getattr(self.libc, 'wcwidth', None) is not None
         assert getattr(self.libc, 'wcswidth', None) is not None
 
-    def _is_equal_wcwidth(self, ucs_chunk):
-        for ucs in ucs_chunk:
-            assert self.libc.wcwidth(ucs) == wcwidth.wcwidth(ucs)
+    def _is_equal_wcwidth(self, ucs):
+        w_libc, w_local = self.libc.wcwidth(ucs), wcwidth.wcwidth(ucs)
+        assert w_libc == w_local, report_ucs_msg(ucs, w_libc, w_local)
 
-    def test_libc_wcwidth_equality(self, step=1000):
+    def test_libc_wcwidth_equality(self):
         """
         Compare libc.wcwidth and wcwidth.wcwidth in chunks of ``step``,
         so that py.test generates test "blocks" (we have a lot of failures!)
         """
         locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
 
-        for val in range(0, LIMIT_UCS, step):
-            ucs_chunk = [unichr(val) for val in range(val, val+step)]
-            yield self._is_equal_wcwidth, ucs_chunk
+        for ucs in ALL_UCS:
+            yield self._is_equal_wcwidth, ucs
 
-    def _is_equal_wcswidth(self, ucs_chunk):
-        for a, b in itertools.combinations(range(len(ucs_chunk)), 2):
-            idx = slice(a, b) if a < b else slice(b, a)
-            ucs = ucs_chunk[idx]
-            assert self.libc.wcswidth(ucs, len(ucs)) == wcwidth.wcswidth(ucs)
-
-    def test_libc_wcswidth_equality(self, step=50):
-        """
-        Compare libc.wcswidth and wcwidth.wcswidth in chunks of ``step``,
-        so that py.test generates test "blocks" (we have a lot of failures!)
-        """
-        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
-
-        for val in range(0, LIMIT_UCS, step):
-            (start, end) = (val, val + step)
-            ucs = u''.join([unichr(_num) for _num in range(start, end)])
-            yield self._is_equal_wcswidth, ucs
+#    def _is_equal_wcswidth(self, ucs_chunk):
+#        for a, b in itertools.combinations(range(len(ucs_chunk)), 2):
+#            idx = slice(a, b) if a < b else slice(b, a)
+#            ucs = ucs_chunk[idx]
+#            assert self.libc.wcswidth(ucs, len(ucs)) == wcwidth.wcswidth(ucs)
+#
+#    def disabled_test_libc_wcswidth_equality(self, step=50):
+#        """
+#        Compare libc.wcswidth and wcwidth.wcswidth in chunks of ``step``,
+#        so that py.test generates test "blocks" (we have a lot of failures!)
+#        """
+#        locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8'))
+#
+#        for val in range(0, LIMIT_UCS, step):
+#            (start, end) = (val, val + step)
+#            ucs = u''.join([unichr(_num) for _num in range(start, end)])
+#            yield self._is_equal_wcswidth, ucs
