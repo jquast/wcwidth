e1f2fa89911f72ad5d33502950976231313b0861
Jeff Quast <contact@jeffquast.com>
2020-05-30 22:50:34 -0400

    reallocating binomial callbacks

---
+++ bin/run_codecov.py
@@ -0,0 +1,25 @@
+"""Workaround for https://github.com/codecov/codecov-python/issues/158."""
+# std imports
+import sys
+import time
+
+# 3rd party
+import codecov
+import tenacity
+
+tenacity.retry(wait=tenacity.wait_random(min=1, max=5),
+               stop=tenacity.stop_after_delay(60))
+
+
+def main():
+    """Run codecov up to RETRIES times On the final attempt, let it exit normally."""
+
+    # Make a copy of argv and make sure --required is in it
+    args = sys.argv[1:]
+    if '--required' not in args:
+        args.append('--required')
+    codecov.main(*args)
+
+
+if __name__ == '__main__':
+    main()
