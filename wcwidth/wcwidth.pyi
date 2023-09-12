07148241c2a3e3b1332e383ee3564e43fd9b1ba9
Trim21 <trim21.me@gmail.com>
2023-09-13 01:58:17 +0800

    fix mypy

---
+++ wcwidth/wcwidth.pyi
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+def wcwidth(wc: str, unicode_version: str = ...) -> int:
+    ...
+
+
+def wcswidth(pwcs: str, n: Optional[int] = None, unicode_version: str = ...):
+    ...
+
+
+def _bisearch(ucs: int, table: list[tuple[int, int]]) -> int:
+    ...
+
+
+def _wcversion_value(ver_string: str) -> tuple[int, ...]:
+    ...
+
+
+def _wcmatch_version(given_version: str) -> str:
+    ...
+
+
+__version__: str = ...
