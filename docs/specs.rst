b40fbf356a5eb68794d488c0669e32037b6d793d
Jeff Quast <contact@jeffquast.com>
2023-10-19 16:53:49 -0400

    backport tests to python2.7 and "narrow" builds

---
+++ docs/specs.rst
@@ -0,0 +1,50 @@
+.. _Specification:
+
+=============
+Specification
+=============
+
+This document defines how the wcwidth library measures the printable width of characters of a string.
+
+Width of -1
+-----------
+
+The following have a column width of -1 for only the POSIX-like
+functions :func:`wcwidth.wcwidth` and :func:`wcwidth.wcswidth`:
+
+- C0 control characters (U+001 through U+01F).
+- C1 control characters and DEL (U+07F through U+0A0).
+
+Width of 0
+----------
+
+Any characters defined by category codes in DerivedGeneralCategory txt files:
+
+- 'Me': Enclosing Combining Mark, aprox. 13 characters.
+- 'Mn': Nonspacing Combining Mark, aprox. 1,839 characters.
+- 'Mc': Spacing Mark, aprox. 443 characters.
+- 'Cf': Format control character, aprox. 161 characters.
+- 'Zl': U+2028 LINE SEPARATOR only
+- 'Zp': U+2029 PARAGRAPH SEPARATOR only
+- 'Sk': Modifier Symbol, aprox. 4 characters of only those where phrase ``'EMOJI
+  MODIFIER'`` is present in comment of unicode data file.
+
+The NULL character (``U+0000``).
+
+Any character following a ZWJ (``U+200D``) when in sequence by
+function :func:`wcwidth.wcswidth`.
+
+Width of 1
+----------
+
+String characters are measured width of 1 when they are not
+measured as `Width of 0`_ or `Width of 2`.
+
+Width of 2
+----------
+
+Any character defined by East Asian Fullwidth (``F``) or Wide (``W``) properties
+in EastAsianWidth txt files, except those that are defined by the Category codes
+of Nonspacing Mark (``Mn``), Spacing Mark (``Mc``), and the Modifier Symbol
+category, ``'Sk'`` where ``'EMOJI MODIFIER'`` is present in comment of unicode
+data file which are defined as `Width of 0`_.
--- tests/test_core.py
+++ tests/test_core.py
@@ -10,6 +10,24 @@ except ImportError:
 # local
 import wcwidth
 
+# 3rd party
+import pytest
+
+# some tests cannot be done on some builds of python, where the internal
+# unicode structure is limited to 0x10000 for memory conservation,
+# "ValueError: unichr() arg not in range(0x10000) (narrow Python build)"
+try:
+    # python 2
+    _ = unichr
+except NameError:
+    # python 3
+    unichr = chr
+try:
+    unichr(0x2fffe)
+    NARROW_ONLY = False
+except ValueError:
+    NARROW_ONLY = True
+
 
 def test_package_version():
     """wcwidth.__version__ is expected value."""
@@ -21,6 +39,25 @@ def test_package_version():
 
     # verify.
     assert result == expected
+
+def basic_string_type():
+    """
+    This is a python 2-specific test of the basic "string type"
+
+    Such strings cannot contain anything but ascii in python2.
+    """
+    # given,
+    phrase = 'hello\x00world'
+    expect_length_each = (1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1)
+    expect_length_phrase = sum(expect_length_each)
+
+    # exercise,
+    length_each = tuple(map(wcwidth.wcwidth, phrase))
+    length_phrase = wcwidth.wcswidth(phrase)
+
+    # verify.
+    assert length_each == expect_length_each
+    assert length_phrase == expect_length_phrase
 
 
 def test_hello_jp():
@@ -140,7 +177,7 @@ def test_combining_cafe():
 
 
 def test_combining_enclosing():
-    u"""CYRILLIC CAPITAL LETTER A + COMBINING CYRILLIC HUNDRED THOUSANDS SIGN is А҈ of length 1."""
+    u"""CYRILLIC CAPITAL LETTER A + COMBINING CYRILLIC HUNDRED THOUSANDS SIGN is of length 1."""
     phrase = u"\u0410\u0488"
     expect_length_each = (1, 0)
     expect_length_phrase = 1
@@ -155,7 +192,12 @@ def test_combining_enclosing():
 
 
 def test_balinese_script():
-    u"""Balinese kapal (ship) is ᬓᬨᬮ᭄ of length 4."""
+    u"""
+    Balinese kapal (ship) is length 3.
+    
+    This may be an example that is not yet correctly rendered by any terminal so
+    far, like devanagari.
+    """
     phrase = (u"\u1B13"    # Category 'Lo', EAW 'N' -- BALINESE LETTER KA
               u"\u1B28"    # Category 'Lo', EAW 'N' -- BALINESE LETTER PA KAPAL
               u"\u1B2E"    # Category 'Lo', EAW 'N' -- BALINESE LETTER LA
@@ -180,7 +222,7 @@ def test_kr_jamo_filler():
     like it, ``\uffa0``, ``\u1160``, ``\u115f``, ``\u1160``, are not commonly viewed with a terminal,
     seems it doesn't matter whether it is implemented or not, they are not typically used !
     """
-    phrase = "\u1100\u1160"
+    phrase = u"\u1100\u1160"
     expect_length_each = (2, 1)
     expect_length_phrase = 3
 
@@ -192,6 +234,7 @@ def test_kr_jamo_filler():
     assert length_each == expect_length_each
     assert length_phrase == expect_length_phrase
 
+@pytest.mark.skipif(NARROW_ONLY, reason="Test cannot verify on python 'narrow' builds")
 def emoji_zwj_sequence():
     u"""
     Emoji zwj sequence of four codepoints is just 2 cells.
@@ -212,24 +255,66 @@ def emoji_zwj_sequence():
     assert length_each == expect_length_each
     assert length_phrase == expect_length_phrase
 
-def longer_emoji_zwj_sequence():
+@pytest.mark.skipif(NARROW_ONLY, reason="Test cannot verify on python 'narrow' builds")
+def test_unfinished_zwj_sequence():
     u"""
+    Ensure index-out-of-bounds does not occur for zero-width joiner without any following character
+    """
+    phrase = (u"\U0001f469"   # Base, Category So, East Asian Width property 'W' -- WOMAN
+              u"\U0001f3fb"   # Modifier, Category Sk, East Asian Width property 'W' -- EMOJI MODIFIER FITZPATRICK TYPE-1-2
+              u"\u200d")      # Joiner, Category Cf, East Asian Width property 'N'  -- ZERO WIDTH JOINER
+    expect_length_each = (2, 0, 0)
+    expect_length_phrase = 2
+
+    # exercise,
+    length_each = tuple(map(wcwidth.wcwidth, phrase))
+    length_phrase = wcwidth.wcswidth(phrase)
+
+    # verify.
+    assert length_each == expect_length_each
+    assert length_phrase == expect_length_phrase
+ 
+
+@pytest.mark.skipif(NARROW_ONLY, reason="Test cannot verify on python 'narrow' builds")
+def test_non_recommended_zwj_sequence():
+    """
+    Verify ZWJ is measured as though successful with characters that cannot be joined, wcwidth does not verify
+    """
+    phrase = (u"\U0001f469"   # Base, Category So, East Asian Width property 'W' -- WOMAN
+              u"\U0001f3fb"   # Modifier, Category Sk, East Asian Width property 'W' -- EMOJI MODIFIER FITZPATRICK TYPE-1-2
+              u"\u200d")      # Joiner, Category Cf, East Asian Width property 'N'  -- ZERO WIDTH JOINER
+    expect_length_each = (2, 0, 0)
+    expect_length_phrase = 2
+
+    # exercise,
+    length_each = tuple(map(wcwidth.wcwidth, phrase))
+    length_phrase = wcwidth.wcswidth(phrase)
+
+    # verify.
+    assert length_each == expect_length_each
+    assert length_phrase == expect_length_phrase
+ 
+
+
+@pytest.mark.skipif(NARROW_ONLY, reason="Test cannot verify on python 'narrow' builds")
+def test_longer_emoji_zwj_sequence():
+    """
     A much longer emoji ZWJ sequence of 10 total codepoints is just 2 cells!
     """
     # 'Category Code', 'East Asian Width property' -- 'description'
     phrase = (u"\U0001F9D1"   # 'So', 'W' -- ADULT
               u"\U0001F3FB"   # 'Sk', 'W' -- EMOJI MODIFIER FITZPATRICK TYPE-1-2
               u"\u200d"       # 'Cf', 'N' -- ZERO WIDTH JOINER
-              u"\u2764"       # 'So', 'W' -- HEAVY BLACK HEART
+              u"\u2764"       # 'So', 'N' -- HEAVY BLACK HEART
               u"\uFE0F"       # 'Mn', 'A' -- VARIATION SELECTOR-16
               u"\u200d"       # 'Cf', 'N' -- ZERO WIDTH JOINER
-              u"\U0001F48B"   # 'So', 'N' -- KISS MARK
+              u"\U0001F48B"   # 'So', 'W' -- KISS MARK
               u"\u200d"       # 'Cf', 'N' -- ZERO WIDTH JOINER
               u"\U0001F9D1"   # 'So', 'W' -- ADULT
               u"\U0001F3FD")  # 'Sk', 'W' -- EMOJI MODIFIER FITZPATRICK TYPE-4
 
     # This test adapted from https://www.unicode.org/L2/L2023/23107-terminal-suppt.pdf
-    expect_length_each = (2, 0, 0, 2, 0, 0, 1, 0, 2, 0)
+    expect_length_each = (2, 0, 0, 1, 0, 0, 2, 0, 2, 0)
     expect_length_phrase = 2
 
     # exercise,
@@ -241,7 +326,7 @@ def longer_emoji_zwj_sequence():
     assert length_phrase == expect_length_phrase
 
 def test_devanagari_script():
-    u"""
+    """
     Attempt to test the measurement width of Devanagari script.
 
     I believe this 'phrase' should be length 3.
@@ -313,10 +398,10 @@ def test_kannada_script():
     # This test adapted from https://www.unicode.org/L2/L2023/23107-terminal-suppt.pdf
     # |ರ್ಝೈ|
     # |123|
-    phrase = ("\u0cb0"    # Repha, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER RA
-              "\u0ccd"    # Joiner, Category 'Mn', East Asian Width property 'N' -- KANNADA SIGN VIRAMA
-              "\u0c9d"    # Base, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER JHA
-              "\u0cc8")   # MatraUR, Category 'Mc', East Asian Width property 'N' -- KANNADA VOWEL SIGN AI
+    phrase = (u"\u0cb0"    # Repha, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER RA
+              u"\u0ccd"    # Joiner, Category 'Mn', East Asian Width property 'N' -- KANNADA SIGN VIRAMA
+              u"\u0c9d"    # Base, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER JHA
+              u"\u0cc8")   # MatraUR, Category 'Mc', East Asian Width property 'N' -- KANNADA VOWEL SIGN AI
     # 23107-terminal-suppt.pdf suggests should be (2, 0, 3, 1)
     expect_length_each = (1, 0, 1, 0)
     # I believe the correct final width *should* be 3 or 4.
@@ -335,10 +420,10 @@ def test_kannada_script_2():
     # This test adapted from https://www.unicode.org/L2/L2023/23107-terminal-suppt.pdf
     # |ರ಼್ಚ|
     # |12|
-    phrase = ("\u0cb0"    # Base, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER RA
-              "\u0cbc"    # Nukta, Category 'Mn', East Asian Width property 'N' -- KANNADA SIGN NUKTA
-              "\u0ccd"    # Joiner, Category 'Lo', East Asian Width property 'N' -- KANNADA SIGN VIRAMA
-              "\u0c9a")   # Subjoin, Category 'Mc', East Asian Width property 'N' -- KANNADA LETTER CA
+    phrase = (u"\u0cb0"    # Base, Category 'Lo', East Asian Width property 'N' -- KANNADA LETTER RA
+              u"\u0cbc"    # Nukta, Category 'Mn', East Asian Width property 'N' -- KANNADA SIGN NUKTA
+              u"\u0ccd"    # Joiner, Category 'Lo', East Asian Width property 'N' -- KANNADA SIGN VIRAMA
+              u"\u0c9a")   # Subjoin, Category 'Mc', East Asian Width property 'N' -- KANNADA LETTER CA
     # 23107-terminal-suppt.pdf suggests wcwidth.wcwidth should return (2, 0, 0, 1)
     expect_length_each = (1, 0, 0, 1)
     # I believe the final width is correct, but maybe for the wrong reasons!
