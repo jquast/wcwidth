85b39b0b10c543035bae9804e9e876c89371fa03
Trim21 <trim21.me@gmail.com>
2023-11-21 20:44:00 +0800

    fix typing

--- wcwidth/wcwidth.pyi
+++ wcwidth/wcwidth.pyi
@@ -1,12 +1,23 @@
-from typing import Optional
+from typing import Optional, AnyStr
+
+import sys
+
+if sys.version_info >= (3,):
+    def wcwidth(wc: str, unicode_version: str = ...) -> int:
+        ...
 
 
-def wcwidth(wc: str, unicode_version: str = ...) -> int:
-    ...
+    def wcswidth(pwcs: str, n: Optional[int] = None, unicode_version: str = ...):
+        ...
+else:
+    def wcwidth(wc: AnyStr, unicode_version: str = ...) -> int:
+        ...
 
 
-def wcswidth(pwcs: str, n: Optional[int] = None, unicode_version: str = ...):
-    ...
+    def wcswidth(pwcs: AnyStr, n: Optional[int] = None, unicode_version: str = ...):
+        ...
+
+
 
 
 def _bisearch(ucs: int, table: list[tuple[int, int]]) -> int:
