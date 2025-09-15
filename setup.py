ceba6d6b683ed713a020054ff8a7747f79b2449b
user <grayjk@gmail.com>
2025-09-15 10:53:44 -0400

    add Python 3.13

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ def main():
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
             'Programming Language :: Python :: 3.12',
+            'Programming Language :: Python :: 3.13',
             'Topic :: Software Development :: Libraries',
             'Topic :: Software Development :: Localization',
             'Topic :: Software Development :: Internationalization',
