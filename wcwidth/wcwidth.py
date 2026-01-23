a7c6571a95faac0f21743f5a3731a5caf9c97e51
Jeff Quast <contact@jeffquast.com>
2026-01-23 12:41:17 -0500

    According to isort, we need more empty lines ..

--- wcwidth/wcwidth.py
+++ wcwidth/wcwidth.py
@@ -87,6 +87,7 @@ from .unicode_versions import list_versions
 if TYPE_CHECKING:  # pragma: no cover
     # std imports
     from collections.abc import Iterator
+
     from typing import Literal
 
 _AMBIGUOUS_TABLE = AMBIGUOUS_EASTASIAN[next(iter(AMBIGUOUS_EASTASIAN))]
